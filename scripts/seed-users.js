456a1b8e273d823fde0957982ac174fdec2b0bc7
breduffy <breanne.duffy@gmail.com>
2015-06-23 16:54:28 -0400

    Create seed post data, fix schemas
    * Create seed post data in order to test schemas
    * Fix schema nesting (was previously nest models not schemas)

--- scripts/seed-users.js
+++ scripts/seed-users.js
@@ -5,26 +5,27 @@ mongoose.connect('mongodb://localhost/nope');
 
 var User = require('../lib/users.js');
 
-var createUserOne = function(done){
+var createUserOne = function(done) {
   User.create({
     userName: 'courtney',
     name: 'courtney',
     email: 'courtneys@gmail.com',
     bio: 'I like going to the pool and chocolate milk',
-    profilePic: 'https://scontent.xx.fbcdn.net/hphotos-xaf1/v/t1.0-9/382991_2353714370232_1390898971_n.jpg?oh=460af6f829b22f7648fcc4a3b0e65215&oe=5625EE04'
+    profilePic: 'https://scontent.xx.fbcdn.net/hphotos-xaf1/v/t1.0-9/382991_2353714370232_1390898971_n.jpg?oh=460af6f829b22f7648fcc4a3b0e65215&oe=5625EE04',
+    posts: []
   }, done);
 };
 
 async.series([
-  createUserOne
+    createUserOne
   ],
-    function(err){
-      if(err){
-        console.log(err);
-      }
-      mongoose.disconnect();
+  function(err) {
+    if (err) {
+      console.log(err);
     }
-  );
+    mongoose.disconnect();
+  }
+);
 
 
 // var userSchema = new mongoose.Schema({

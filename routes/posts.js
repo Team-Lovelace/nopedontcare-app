b0a8949683570d4d22e8f460c261f8606ba11fc3
courtneysadusky <courtneysadusky@gmail.com>
2015-06-26 10:48:07 -0400

    add post route to posts.js

--- routes/posts.js
+++ routes/posts.js
@@ -15,4 +15,28 @@ router.get('/:username/nopes', function(req, res){
   });
 });
 
+router.post('/:username/nopes', jsonParser);
+router.post('/:username/nopes', function(req, res){
+  User.findOne({userName: req.params.username}, function(error, user){
+    if (error){
+      console.log(error);
+    }
+    post = new Post({
+      author:user._id
+    });
+    post.save(function(error){
+      if (error){
+        console.error(error);
+      }
+      user.posts.push(post._id);
+      user.save(function(error){
+        if (error){
+        console.error(error);
+        }
+        res.json(post);
+        });
+     });
+  });
+});
+
 module.exports = router;

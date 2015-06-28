c62beb9fa0938035883473561205622c7e1b6da9
courtneysadusky <courtneysadusky@gmail.com>
2015-06-27 22:19:38 -0400

    add get route to comments.js to get comments by post id

--- routes/comments.js
+++ routes/comments.js
@@ -1,13 +1,37 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../lib/users.js');
+var Post = require('../lib/posts.js');
+var Comment = require('../lib/comments.js');
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
 
 router.get('/', function(req, res) {
   console.log("user!");
 });
 
 router.get('/:username/nopes/:id/comments', function(req, res) {
-  console.log("We got it");
+  Post.find({_id: req.params.id})
+  .populate('comments')
+  .exec(function(error, comment){
+   console.log(comment);
+   res.json(comment);
+  });
 });
+
+// router.get('/:username/nopes/:id/comments', function(req, res){
+//   User.findOne({userName: req.params.username}, function(error, user){
+//     if (error){
+//       console.error(error);
+//     }
+//     Post.find({_id: req.params.id})
+//     .populate('comments')
+//     .exec(function(error, comment){
+//       console.log(comment);
+//       res.json(comment);
+//     });
+//   })
+// });
 
 router.post('/:username/nopes/:id/comments', function(req, res) {
   console.log("We posted it");

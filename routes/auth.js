4976037bf2954ebb95cf11f5ea824175afe8f462
Peter Masucci <peter.a.masucci@gmail.com>
2015-06-29 07:01:30 -0400

    Removed FB and Twitter auth.
    Falling back to local user registration/authentication.

--- routes/auth.js
+++ routes/auth.js
@@ -1,30 +1,32 @@
 var passport = require('passport');
-var User = require('../lib/users.js');
+var Account = require('../models/account');
 var express = require('express');
 var router = express.Router();
+
+router.route('/register')
+  .get(function(req, res, next) {
+    res.render('register', {});
+  })
+  .post(function(req, res, next) {
+    Account.register(new Account({username: req.body.username}), req.body.password, function(err, account) {
+      if(err) {
+        return res.render('register', {account: account});
+      }
+
+      req.login(account, function(err) {
+        res.redirect('/contacts');
+      });
+    })
+  })
+
+router.get('/login', function(req, res, next) {
+  res.render('login', {user: req.user});
+});
 
 router.all('/logout', function(req, res, next) {
   req.logout();
   res.redirect('/');
 });
 
-router.get('/', function(req, res, next) {
-  res.render('login-options', {user: req.user});
-});
-
-
-router.get('/facebook', passport.authenticate('facebook'));
-
-router.get('/facebook/callback', passport.authenticate('facebook', {
-  successRedirect: '/contacts',
-  failureRedirect: '/auth'
-}));
-
-router.get('/twitter', passport.authenticate('twitter'));
-
-router.get('/twitter/callback', passport.authenticate('twitter', {
-  successRedirect: '/contacts',
-  failureRedirect: '/auth'
-}));
 
 module.exports = router;

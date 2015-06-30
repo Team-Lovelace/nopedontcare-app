506cb7b967efb214fa26e44e5bf2aa2742e41023
breduffy <breanne.duffy@gmail.com>
2015-06-29 20:13:42 -0400

    Picture upload working. Still needs to be added to template.

--- app.js
+++ app.js
@@ -7,6 +7,15 @@ var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var jsonParser = bodyParser.json();
 var passport = require('passport');
+var AWS = require('aws-sdk');
+require('dotenv').load();
+
+AWS.config.update({
+  accessKeyId: process.env.ACCESS_KEY_ID,
+  secretAccessKey: process.env.SECRET_ACCESS_KEY_ID
+});
+
+var s3 = new AWS.S3();
 
 var LocalStrategy = require('passport-local').Strategy;
 
@@ -59,43 +68,7 @@ var posts = require('./routes/posts.js');
 var auth = require('./routes/auth.js');
 var moment = require('moment');
 
-app.post('/nopes', jsonParser);
-app.post('/nopes', function(req, res) {
-  User.findOne({
-    username: req.user.username
-  }, function(error, user) {
-    if (error) {
-      console.log(error);
-    }
-    var post = new Post({
-      author: user._id,
-      caption: req.body.caption,
-      pubDate: moment().format()
-    });
-    post.save(function(error) {
-      if (error) {
-        console.error(error);
-      }
-      user.posts.push(post._id);
-      user.save(function(error) {
-        if (error) {
-          console.error(error);
-          return res.sendStatus(400);
-        }
-        fs.readFile('./templates/post-template.jade', 'utf8', function(err, data) {
-          if (err) {
-            res.sendStatus(400);
-          };
-          post.username = user.username;
-          var postCompiler = jade.compile(data);
-          var html = postCompiler(post);
-          res.send(html);
-          res.status(201);
-        });
-      });
-    });
-  });
-});
+
 
 
 /* USERS ROUTE FOR DEV PURPOSES ONLY */
@@ -189,38 +162,6 @@ app.post('/register', function(req, res) {
   });
 });
 
-//Multer Image Upload
-/*Define dependencies.*/
-var multer = require('multer');
-var app = express();
-var done = false;
-
-app.use(multer({
-  dest: './uploads/',
-  rename: function(fieldname, filename) {
-    return filename + Date.now();
-  },
-  onFileUploadStart: function(file) {
-    console.log(file.originalname + ' is starting ...')
-  },
-  onFileUploadComplete: function(file) {
-    console.log(file.fieldname + ' uploaded to  ' + file.path)
-    done = true;
-  }
-}));
-
-app.get('/', function(req, res) {
-  res.sendfile("index.html");
-});
-
-app.post('/api/photo', function(req, res) {
-  if (done == true) {
-    console.log(req.files);
-    res.end("File uploaded.");
-  }
-});
-//END multer
-
 //using an express method, static
 //that creates middlewear that serves up static files
 //along with our html
@@ -236,6 +177,98 @@ app.use('/user/', comments);
 app.use('/user/', users);
 app.use('/user/', posts);
 app.use('/auth/', auth);
+
+//Multer Image Upload
+/*Define dependencies.*/
+var multer = require('multer');
+var done = false;
+
+/// move me downstream!!!!!!!!!!!
+app.use(multer({
+  dest: './uploads/',
+  rename: function(pictures, src) {
+    return src.replace(/\W+/g, '-').toLowerCase();
+  },
+  onFileUploadStart: function(file) {
+    console.log(file.originalname + ' is starting ...')
+  },
+  onFileUploadComplete: function(file) {
+    console.log(file.fieldname + ' uploaded to  ' + file.path)
+    done = true;
+  }
+}));
+
+// app.post('/nopes', jsonParser);
+
+app.post('/nopes', function(req, res, next) {
+  if (done == true) {
+    next();
+  }
+});
+
+app.post('/nopes', function(req, res, next) {
+  fs.readFile(req.files.file.path, function(err, data) {
+    var params = {
+      Key: req.files.file.name,
+      Body: data,
+      Bucket: 'nopes'
+    };
+    s3.putObject(params, function(perr, pres) {
+      if (perr) {
+        console.log(perr)
+        res.sendStatus(400);
+      } else {
+        if (!req.locals) {
+          req.locals = {};
+          req.locals.pictureSrc = req.files.file.name;
+          next();
+        } else {
+          req.locals.pictureSrc = req.files.file.name;
+          next();
+        }
+      }
+    });
+  });
+});
+
+app.post('/nopes', function(req, res) {
+  User.findOne({
+    username: req.user.username
+  }, function(error, user) {
+    if (error) {
+      console.log(error);
+    }
+    var post = new Post({
+      author: user._id,
+      picture: req.locals.pictureSrc,
+      caption: req.body.caption,
+      pubDate: moment().format()
+    });
+    post.save(function(error) {
+      if (error) {
+        console.error(error);
+      }
+      user.posts.push(post._id);
+      user.save(function(error) {
+        if (error) {
+          console.error(error);
+          return res.sendStatus(400);
+        }
+        fs.readFile('./templates/post-template.jade', 'utf8', function(err, data) {
+          if (err) {
+            res.sendStatus(400);
+          };
+          post.username = user.username;
+          var postCompiler = jade.compile(data);
+          var html = postCompiler(post);
+          res.send(html);
+          res.status(201);
+        });
+      });
+    });
+  });
+});
+// END MOVE ME DOWNSTREAM!!!!
 
 //app variable is used to listen but not as variable
 var server = app.listen(Port, function() {
